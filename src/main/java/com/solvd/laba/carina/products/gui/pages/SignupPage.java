5c967df3e559ab8ceba2cecd5b46a6739fa9c7d4
hhan-solvd <130609200+hhan-solvd@users.noreply.github.com>
2023-07-23 10:21:45 -0500

    Update src/main/java/com/solvd/laba/carina/products/gui/pages/SignupPage.java
    Co-authored-by: ankochergin <103670717+ankochergin@users.noreply.github.com>

--- src/main/java/com/solvd/laba/carina/products/gui/pages/SignupPage.java
+++ src/main/java/com/solvd/laba/carina/products/gui/pages/SignupPage.java
@@ -97,7 +97,6 @@ public class SignupPage extends AbstractPage {
 
     public AccountCreatedPage clickCreateAccountButton() {
         createAccountButton.click();
-
         return new AccountCreatedPage(driver);
     }
 
