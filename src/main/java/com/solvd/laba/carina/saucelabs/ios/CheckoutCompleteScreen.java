f02200bec94208ae9e87948dbd6a1a93382bff87
hhan-solvd <hhan.laba@solvd.com>
2023-07-31 10:36:03 -0500

    Implemented iOS screens

--- src/main/java/com/solvd/laba/carina/saucelabs/ios/CheckoutCompleteScreen.java
+++ src/main/java/com/solvd/laba/carina/saucelabs/ios/CheckoutCompleteScreen.java
@@ -3,10 +3,18 @@ package com.solvd.laba.carina.saucelabs.ios;
 import com.solvd.laba.carina.saucelabs.common.CheckoutCompleteScreenBase;
 import com.zebrunner.carina.utils.factory.DeviceType;
 import com.zebrunner.carina.utils.factory.DeviceType.Type;
+import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
+import com.zebrunner.carina.webdriver.locator.ExtendedFindBy;
 import org.openqa.selenium.WebDriver;
 
 @DeviceType(pageType = Type.IOS_PHONE, parentClass = CheckoutCompleteScreenBase.class)
 public class CheckoutCompleteScreen extends CheckoutCompleteScreenBase {
+
+    @ExtendedFindBy(iosClassChain = "**/XCUIElementTypeStaticText[`label == \"CHECKOUT: COMPLETE!\"`]")
+    private ExtendedWebElement screenTitle;
+
+    @ExtendedFindBy(iosClassChain = "**/XCUIElementTypeStaticText[`label == \"THANK YOU FOR YOU ORDER\"`]")
+    private ExtendedWebElement successMessage;
 
     public CheckoutCompleteScreen(WebDriver driver) {
         super(driver);
@@ -14,12 +22,12 @@ public class CheckoutCompleteScreen extends CheckoutCompleteScreenBase {
 
     @Override
     public boolean isOpened() {
-        throw new UnsupportedOperationException(METHOD_IS_NOT_IMPLEMENTED_FOR_IOS);
+        return screenTitle.isElementPresent();
     }
 
     @Override
     public boolean isSuccessMessageDisplayed() {
-        throw new UnsupportedOperationException(METHOD_IS_NOT_IMPLEMENTED_FOR_IOS);
+        return successMessage.isElementPresent();
     }
 
 }
